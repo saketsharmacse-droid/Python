950a30dc2228d83678e1c9dfa1e4f78b92db39eb
saketsharmacse-droid <saket.sharma.cse@gmail.com>
2025-09-27 10:33:55 +0530

    Initial commit

---
+++ 00_python/FirstProgram.py
@@ -0,0 +1,10 @@
+
+name = " saket"
+age = 22
+price = 234.677
+print(age)
+print("My name is : ", name)
+print("My Age is:", age)
+print(" the price of commodity is: ", price)
+print(type(age))
+print(type(price))
